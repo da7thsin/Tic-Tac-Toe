4517f81ca52cb7c5843f48c59dafb947a948d276
Geoffrey <geo.galagaran@gmail.com>
2016-11-15 11:10:58 +0800

    Added Game and Player constructors

--- game.js
+++ game.js
@@ -1,3 +1,35 @@
-$(document).ready(function(){
 
+function Game(player){
+  var finished = false;
+  var start = false;
+
+  this.win_combo = [
+    [0,1,2],[3,4,5],[6,7,8], //horizontals
+    [0,3,6],[1,4,7],[2,5,8], //verticals
+    [0,4,8],[3,4,6] //diagonals
+  ];
+
+  this.board = document.getElementsByClassName('grid');
+
+  this.checkBoard = function(){
+
+  }
+
+  this.sign = (function(player){
+    return player.sign == 'X'?'O':'X';
+  })(player);
+
+  this.turn = false;
+}
+
+function Player(sign){
+  this.sign = sign;
+  this.turn = false;
+}
+
+var player = new Player('X');
+var game = new Game(player);
+
+$('.grid').click(function(){
+  console.log(game.board);
 });
